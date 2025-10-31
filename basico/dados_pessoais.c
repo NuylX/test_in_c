ddfbbfd2169a548567ed390d82199524e5937a76
NuylX <brunojuancosta04@gmail.com>
2025-10-30 21:26:42 -0400

    Inicializa variáveis com valores padrão e mantém a estrutura de impressão dos dados pessoais

--- basico/dados_pessoais.c
+++ basico/dados_pessoais.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    char nome[50];
-    int idade;
-    float altura;
+    char nome[50] = "User";
+    int idade = 0;
+    float altura = 0.0;
 
-    printf("Nome: %s\nAltura: %.2f\nIdade: %d anos\n", nome, idade, altura);
+    //printf("Digite o seu nome: ");
+    //scanf("%s", &nome);
 
+    //printf("Digite a sua idade: ");
+    //scanf("%d", &idade);
+
+    //printf("Digite a sua altura (Digite em metros): ");
+    //scanf("%f", &altura);
+
+    //printf("Dados Pessoais: \n");
+    printf("Nome: %s\nIdade: %d\nAltura: %.2f\n", nome, idade, altura);
     return 0;
 }
